0927d727451012ebeda361edd5320f70f906d8d0
diefesson <diefesson.so@gmail.com>
2024-09-30 11:30:54 -0300

    Added Powah, restored Extreme Reactors

---
+++ kubejs/server_scripts/bigreactors/tags.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('item', event => {
+    // Ores
+    event.add('forge:ores/yellorium', 'bigreactors:deepslate_yellorite_ore')
+})
+ServerEvents.tags('block', event => {
+    // Ores
+    event.add('forge:ores/yellorium', 'bigreactors:deepslate_yellorite_ore')
+})
