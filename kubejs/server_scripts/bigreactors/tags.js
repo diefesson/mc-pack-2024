053ebcab606fa22a67e4d99b3141587ecf05f221
diefesson <diefesson.so@gmail.com>
2024-10-30 16:02:17 -0300

    Reworked Extreme Reactors tags

--- kubejs/server_scripts/bigreactors/tags.js
+++ kubejs/server_scripts/bigreactors/tags.js
@@ -1,8 +1,53 @@
-ServerEvents.tags('item', event => {
-    // Ores
-    event.add('forge:ores/yellorium', 'bigreactors:deepslate_yellorite_ore')
-})
+const YELLORITE_ORE = 'bigreactors:yellorite_ore'
+const DEEPSLATE_YELLORITE = 'bigreactors:deepslate_yellorite_ore'
+const BENITOITE_ORE = 'bigreactors:benitoite_ore'
+const ANGLESITE_ORE = 'bigreactors:anglesite_ore'
+
+const ORES = 'forge:ores'
+const URANIUM_ORES = 'forge:ores/uranium'
+const YELLORITE_ORES = 'forge:ores/yellorite'
+const YELLORIUM_ORES = 'forge:ores/yellorium'
+const BENITOITE_ORES = 'forge:ores/benitoite'
+const ANGLESITE_ORES = 'forge:ores/anglesite'
+
 ServerEvents.tags('block', event => {
-    // Ores
-    event.add('forge:ores/yellorium', 'bigreactors:deepslate_yellorite_ore')
+    // Yellorite Ore
+    event.remove(URANIUM_ORES, YELLORITE_ORE)
+    event.remove(YELLORITE_ORES, YELLORITE_ORE)
+    event.add(YELLORIUM_ORES, YELLORITE_ORE)
+
+    // Deepslate Yellorite Ore
+    event.add(YELLORIUM_ORES, DEEPSLATE_YELLORITE)
+
+    // Benitoite
+    event.add(BENITOITE_ORES, BENITOITE_ORE)
+
+    // Anglesite
+    event.add(ANGLESITE_ORES, ANGLESITE_ORE)
+
+    // Tag
+    event.add(ORES, `#${YELLORIUM_ORES}`)
+    event.add(ORES, `#${BENITOITE_ORES}`)
+    event.add(ORES, `#${ANGLESITE_ORES}`)
+})
+
+ServerEvents.tags('item', event => {
+    // Yellorite Ore
+    event.remove(URANIUM_ORES, YELLORITE_ORE)
+    event.remove(YELLORITE_ORES, YELLORITE_ORE)
+    event.add(YELLORIUM_ORES, YELLORITE_ORE)
+
+    // Deepslate Yellorite Ore
+    event.add(YELLORIUM_ORES, DEEPSLATE_YELLORITE)
+
+    // Benitoite
+    event.add(BENITOITE_ORES, BENITOITE_ORE)
+
+    // Anglesite
+    event.add(ANGLESITE_ORES, ANGLESITE_ORE)
+
+    // Tag
+    event.add(ORES, `#${YELLORIUM_ORES}`)
+    event.add(ORES, `#${BENITOITE_ORES}`)
+    event.add(ORES, `#${ANGLESITE_ORES}`)
 })
