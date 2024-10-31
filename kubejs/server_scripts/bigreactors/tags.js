90eab06219cb6d10174c13f282945929636abe4b
diefesson <diefesson.so@gmail.com>
2024-10-31 19:50:49 -0300

    Updated Extreme Reactors yellorium ingot and dust tags

--- kubejs/server_scripts/bigreactors/tags.js
+++ kubejs/server_scripts/bigreactors/tags.js
@@ -1,10 +1,15 @@
 const YELLORITE_ORE = 'bigreactors:yellorite_ore'
-const DEEPSLATE_YELLORITE = 'bigreactors:deepslate_yellorite_ore'
+const DEEPSLATE_YELLORITE_ORE = 'bigreactors:deepslate_yellorite_ore'
+const YELLORIUM_INGOT = 'bigreactors:yellorium_ingot'
+const YELLORIUM_DUST = 'bigreactors:yellorium_dust'
+
 const BENITOITE_ORE = 'bigreactors:benitoite_ore'
 const ANGLESITE_ORE = 'bigreactors:anglesite_ore'
 
 const ORES = 'forge:ores'
 const URANIUM_ORES = 'forge:ores/uranium'
+const URANIUM_INGOTS = 'forge:ingots/uranium'
+const URANIUM_DUSTS = 'forge:dusts/uranium'
 const YELLORITE_ORES = 'forge:ores/yellorite'
 const YELLORIUM_ORES = 'forge:ores/yellorium'
 const BENITOITE_ORES = 'forge:ores/benitoite'
@@ -17,7 +22,7 @@ ServerEvents.tags('block', event => {
     event.add(YELLORIUM_ORES, YELLORITE_ORE)
 
     // Deepslate Yellorite Ore
-    event.add(YELLORIUM_ORES, DEEPSLATE_YELLORITE)
+    event.add(YELLORIUM_ORES, DEEPSLATE_YELLORITE_ORE)
 
     // Benitoite
     event.add(BENITOITE_ORES, BENITOITE_ORE)
@@ -38,7 +43,13 @@ ServerEvents.tags('item', event => {
     event.add(YELLORIUM_ORES, YELLORITE_ORE)
 
     // Deepslate Yellorite Ore
-    event.add(YELLORIUM_ORES, DEEPSLATE_YELLORITE)
+    event.add(YELLORIUM_ORES, DEEPSLATE_YELLORITE_ORE)
+
+    // Yellorium Ingot
+    event.remove(URANIUM_INGOTS, YELLORIUM_INGOT)
+
+    // Yellorium Dust
+    event.remove(URANIUM_DUSTS, YELLORIUM_DUST)
 
     // Benitoite
     event.add(BENITOITE_ORES, BENITOITE_ORE)
