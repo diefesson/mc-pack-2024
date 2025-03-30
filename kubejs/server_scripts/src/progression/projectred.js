a8484abc31dcb60d14f2b9aabccb36491a95f372
Diefesson <diefesson.so@gmail.com>
2025-03-30 14:58:13 -0300

    Removed CB Microblock items

---
+++ kubejs/server_scripts/src/progression/projectred.js
@@ -0,0 +1,29 @@
+// Hide CB Microblock items
+ServerEvents.tags('item', event => {
+    Item.getList().forEach(item => {
+        if (item.id.startsWith('cb_microblock:')) {
+            event.add('c:hidden_from_recipe_viewers', item.id)
+        }
+    })
+})
+
+// Remove CB Microblock item recipes
+ServerEvents.recipes(event => {
+    Item.getList().forEach(item => {
+        if (item.id.startsWith('cb_microblock:')) {
+            event.remove({ output: item.id })
+        }
+    })
+})
+
+// Alternative Silicon recipe
+ServerEvents.recipes(event => {
+    event.remove('projectred_core:silicon')
+    event.shapeless(
+        'projectred_core:silicon',
+        [
+            '#ae2:knife',
+            'projectred_core:boule'
+        ]
+    )
+})
