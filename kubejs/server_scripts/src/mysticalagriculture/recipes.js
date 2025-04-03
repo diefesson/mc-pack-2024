9d446868fcb40201ea568ce735b1b98a17469b50
Diefesson <diefesson.so@gmail.com>
2025-04-03 18:01:03 -0300

    Removed Mystical Agriculture

--- kubejs/server_scripts/src/mysticalagriculture/recipes.js
+++
@@ -1,3 +0,0 @@
-ServerEvents.recipes(event => {
-    event.remove('mysticalagriculture:diamond_sickle')   
-})
--- kubejs/server_scripts/src/mysticalagriculture/seed_and_crops_tags.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.tags('item', event => {
-    event.add('forge:seeds', '#mysticalagriculture:seeds')
-})
-
-ServerEvents.tags('block', event => {
-    event.add('minecraft:crops', '#mysticalagriculture:crops')
-})
