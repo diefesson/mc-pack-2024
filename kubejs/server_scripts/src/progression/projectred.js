2090779eb3957b51df6092f96983cb38919dc9ee
Diefesson <diefesson.so@gmail.com>
2025-04-04 10:47:54 -0300

    Updated Project Red script

--- kubejs/server_scripts/src/progression/projectred.js
+++ kubejs/server_scripts/src/progression/projectred.js
@@ -16,14 +16,42 @@ ServerEvents.recipes(event => {
     })
 })
 
-// Alternative Silicon recipe
 ServerEvents.recipes(event => {
+    // Updated recipes
+    // - Silicon
     event.remove('projectred_core:silicon')
     event.shapeless(
-        'projectred_core:silicon',
+        '8x projectred_core:silicon',
         [
             '#ae2:knife',
             'projectred_core:boule'
         ]
     )
+
+    // - Electrotine Dust
+    event.shapeless(
+        'projectred_core:electrotine_dust',
+        [
+            '4x minecraft:lapis_lazuli',
+            '4x minecraft:redstone',
+        ],
+    )
+
+    // - Plotting Table
+    event.replaceInput(
+        'projectred_fabrication:plotting_table',
+        '#forge:gems/sapphire',
+        '#forge:gems/diamond'
+    )
+
+    // Unwanted content
+    event.remove('projectred_core:electrotine_silicon_comp')
+    event.remove('projectred_core:electrotine_silicon')
+    event.remove('projectred_core:copper_coil')
+    event.remove('projectred_core:iron_coil')
+    event.remove('projectred_core:gold_coil')
+    event.remove('projectred_core:motor')
+    event.remove('projectred_core:woven_cloth')
+    event.remove('projectred_core:sail')
+    event.remove('projectred_core:draw_plate')
 })
