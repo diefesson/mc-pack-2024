4733bad896e1a5854d49cdf69c30bc0945377833
Diefesson <diefesson.so@gmail.com>
2025-04-04 10:39:46 -0300

    Updated Pneumatriccraft script

--- kubejs/server_scripts/src/progression/pneumaticcraft.js
+++ kubejs/server_scripts/src/progression/pneumaticcraft.js
@@ -7,4 +7,28 @@ ServerEvents.recipes(event => {
         '#c:glass_blocks',
         'thermal:cured_rubber',
     )
+
+    // Ammo recipes
+    // - Minigun Ammo
+    event.remove('pneumaticcraft:gun_ammo')
+    event.shaped(
+        'pneumaticcraft:gun_ammo',
+        [
+            ' L ',
+            'CGC',
+            'CGC',
+        ],
+        {
+            'L': '#forge:ingots/lead',
+            'C': 'pneumaticcraft:ingot_iron_compressed',
+            'G': 'minecraft:gunpowder'
+        }
+    )
+
+    // - Freezing Ammo
+    event.replaceInput(
+        { id: 'pneumaticcraft:gun_ammo_freezing' },
+        'minecraft:ice',
+        'thermal:blizz_rod'
+    )
 })
