991353a24397dd147bd63ec81948ccbfda3841d1
Diefesson <diefesson.so@gmail.com>
2025-04-15 15:36:39 -0300

    Removed dangling scripts

--- kubejs/server_scripts/src/railcraft/alloys.js
+++ kubejs/server_scripts/src/railcraft/alloys.js
@@ -1,15 +1,7 @@
 ServerEvents.recipes(event => {
-    ThermalUtils.setup(event)
     event.remove('railcraft:bronze_ingot_crafted_with_ingots')
     event.remove('railcraft:invar_ingot_crafted_with_ingots')
     event.remove('railcraft:brass_ingot_crafted_with_ingots')
-    ThermalUtils.smelter_alloy(
-        { item: 'railcraft:brass_ingot', count: 2 },
-        [
-            { tag: 'forge:ingots/zinc', count: 1 },
-            { tag: 'forge:ingots/copper', count: 1 },
-        ]
-    )
     event.shapeless(
         '2x railcraft:brass_ingot',
         [
