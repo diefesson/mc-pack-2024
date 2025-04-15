a5c43cbfc9a0b8f7ddd7945d9b2d7467790619e2
Diefesson <diefesson.so@gmail.com>
2025-04-15 16:05:40 -0300

    Rebalanced Elytras

--- kubejs/server_scripts/src/elytra_trims/remove_content.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(event => {
-    // Remove Elytra Trims unwanted recipes
-    event.remove('elytratrims:glow')
-    event.remove('elytratrims:animation')
-    event.remove('elytratrims:patterns')
-})
