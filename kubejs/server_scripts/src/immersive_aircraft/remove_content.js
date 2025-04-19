41a1df088f04b2e89121da2bdd8ecc1ec3791708
Diefesson <diefesson.so@gmail.com>
2025-04-19 18:03:14 -0300

    Removed Immersive Aircraft Gyrodyne

--- kubejs/server_scripts/src/immersive_aircraft/remove_content.js
+++ kubejs/server_scripts/src/immersive_aircraft/remove_content.js
@@ -1,7 +1,9 @@
 ServerEvents.recipes(event => {
+    event.remove('immersive_aircraft:gyrodyne')
     event.remove('immersive_aircraft:gyroscope')
 })
 
 ServerEvents.tags('item', event => {
     event.add('c:hidden_from_recipe_viewers', 'immersive_aircraft:gyroscope')
+    event.add('c:hidden_from_recipe_viewers', 'immersive_aircraft:gyrodyne')
 })
