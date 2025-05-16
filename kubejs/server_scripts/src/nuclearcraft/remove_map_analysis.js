f2249bdc0d07424f0882b90cf6ebbe07f4792063
Diefesson <diefesson.so@gmail.com>
2025-05-16 18:09:59 -0300

    Added Nuclearcraft Neoteric and Rustic Delight

---
+++ kubejs/server_scripts/src/nuclearcraft/remove_map_analysis.js
@@ -0,0 +1,3 @@
+ServerEvents.recipes(event => {
+    event.remove('nuclearcraft:analyzer/filled_map')
+})
---
+++ kubejs/server_scripts/src/nuclearcraft/remove_steel_alloying.js
@@ -0,0 +1,4 @@
+ServerEvents.recipes(event => {
+    event.remove('nuclearcraft:alloy_smelter/dusts_coal-dusts_iron')
+    event.remove('nuclearcraft:alloy_smelter/dusts_coal-ingots_iron')
+})
