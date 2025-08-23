bb573e2aa2ec9aca52f8110a4bd3cb271d263e0a
Diefesson <diefesson.so@gmail.com>
2025-08-23 11:26:25 -0300

    Removed custom yellorium recipe

--- kubejs/server_scripts/src/bigreactors/recipes.js
+++ kubejs/server_scripts/src/bigreactors/recipes.js
@@ -1,12 +1,4 @@
 ServerEvents.recipes(event => {
-    event.shapeless(
-        'bigreactors:yellorium_ingot',
-        [
-            '#forge:ingots/uranium',
-            '#forge:dusts/glowstone'
-        ]
-    )
-
     let removedRecipes = [
         'bigreactors:misc/book/erguide_alt',
         'bigreactors:reactor/basic/controller_ingots_uranium',
