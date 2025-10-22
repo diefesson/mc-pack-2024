10a4584db90c0f6f7fff5f705eae5306e53afda2
Diefesson <diefesson.so@gmail.com>
2025-10-22 16:08:57 -0300

    Updated rod hammering recipes

--- kubejs/server_scripts/src/compat/hammering.js
+++ kubejs/server_scripts/src/compat/hammering.js
@@ -4,6 +4,16 @@ ServerEvents.recipes(event => {
         'immersiveengineering:hammer'
     ]
 
+    let craftingHammers = [
+        'immersiveengineering:hammer',
+    ]
+
+    let rodMaterials = [
+        'iron',
+        'steel',
+        'aluminum'
+    ]
+
     let plateMaterials = [
         'dawnstone',
         'iron',
@@ -30,37 +40,58 @@ ServerEvents.recipes(event => {
         'uranium',
     ]
 
+    let rodPattern = [
+        'TI',
+        ' I',
+    ]
+
+    let platePattern = [
+        'T ',
+        'II',
+    ]
+
+    let dustPattern = [
+        'TI',
+    ]
+
     function removeToolCrafting(event, shape, tools, materials) {
         for (let material of materials) {
             event.remove({ input: tools, output: `#forge:${shape}/${material}` })
         }
     }
 
-    function addToolCrafting(event, inCount, outCount, inShape, outShape, tools, materials) {
+    function addToolCrafting(event, outCount, inShape, outShape, tools, pattern, materials) {
         for (let material of materials) {
             let input = `forge:${inShape}/${material}`
             let output = getPreferred(`forge:${outShape}/${material}`)
             if (output == 'minecraft:air') {
-                console.error(`no valid output for ${inShape}/${material} => ${outShape}/${material}`)
+                console.error(`no valid output for ${outShape}/${material}`)
                 continue
             }
-            event.shapeless(
+            event.shaped(
                 { count: outCount, item: output },
-                []
-                    .concat(Array(inCount).fill({ tag: input }))
-                    .concat([tools])
-
-            ).id(`diefalpha:${inShape}_to_${outShape}_${material}`)
+                pattern,
+                {
+                    T: tools,
+                    I: {tag: input}
+                }
+            ).id(`diefalpha:${material}_${inShape}_to_${outShape}`)
         }
     }
 
+    // Rods from ingots hammering
+    event.remove('immersiveengineering:crafting/stick_iron')
+    event.remove('immersiveengineering:crafting/stick_steel')
+    event.remove('immersiveengineering:crafting/stick_aluminum')
+    addToolCrafting(event, 2, 'ingots', 'rods', craftingHammers, rodPattern, rodMaterials)
+
     // Plates from ingots hammering
     removeToolCrafting(event, 'plates', hammers, plateMaterials)
-    addToolCrafting(event, 2, 1, 'ingots', 'plates', hammers, plateMaterials)
+    addToolCrafting(event, 1, 'ingots', 'plates', craftingHammers, platePattern, plateMaterials)
 
     // Dusts from ores and raw materials
     removeToolCrafting(event, 'dusts', hammers, dustMaterials)
-    addToolCrafting(event, 1, 1, 'ores', 'dusts', hammers, dustMaterials)
-    addToolCrafting(event, 1, 1, 'raw_materials', 'dusts', hammers, dustMaterials)
+    addToolCrafting(event, 1, 'ores', 'dusts', craftingHammers, dustPattern, dustMaterials)
+    addToolCrafting(event, 1, 'raw_materials', 'dusts', craftingHammers, dustPattern, dustMaterials)
 
 })
