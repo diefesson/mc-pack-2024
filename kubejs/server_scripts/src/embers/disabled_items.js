54cca7350e390790588a580741de8ceed7fed9d8
Diefesson <diefesson.so@gmail.com>
2025-10-30 22:47:58 -0300

    Added and configured Caverns and Chasms

---
+++ kubejs/server_scripts/src/embers/disabled_items.js
@@ -0,0 +1,19 @@
+let _disabledEmbersItems = [
+    'embers:silver_sword',
+    'embers:silver_shovel',
+    'embers:silver_axe',
+    'embers:silver_pickaxe',
+    'embers:silver_hoe',
+]
+
+ServerEvents.recipes(event => {
+    for(let item of _disabledEmbersItems){
+        event.remove({output: item})
+    }
+})
+
+ServerEvents.tags('item', event => {
+    for(let item of _disabledEmbersItems){
+        // event.add('c:hidden_from_recipe_viewers', item)
+    }
+})
